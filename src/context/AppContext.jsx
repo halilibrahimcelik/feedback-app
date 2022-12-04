c1732361242bafe36350e40fbe7aca4c7852c197
halilibrahimcelik <halil.celik139@gmail.com>
2022-12-04 13:26:38 +0300

    added put delete methods

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -55,22 +55,33 @@ export const FeedBackProvider = ({ children }) => {
     const data = await response.json();
     setFeedBack((prev) => [data, ...prev]);
   };
-  const handleDelete = (id) => {
-    console.log(id);
-    const newData = feedBack.filter((item) => item.id !== id);
-    setFeedBack(newData);
+  const handleDelete = async (id) => {
+    if (window.confirm("Are you sure you have done this ?")) {
+      await fetch(`/feedback/${id}`, {
+        method: "DELETE",
+      });
+      const newData = feedBack.filter((item) => item.id !== id);
+      setFeedBack(newData);
+    }
   };
   const handleEdit = (id) => {
     const selecteddata = feedBack.filter((item) => item.id === id);
     setFeedBackEdit({ item: selecteddata[0], edit: true });
   };
-  const handleUpdate = (id, newItem) => {
+  const handleUpdate = async (id, newItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    });
+    const data = await response.json();
     const { item } = feedBackEdit;
     setFeedBack(
-      feedBack.map((item) => (item.id === id ? { ...item, ...newItem } : item))
+      feedBack.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
-  console.log(feedBackEdit);
 
   const contextValue = {
     loading: loading,
